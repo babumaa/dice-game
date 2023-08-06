5b9172656e37b10efa3f049bae856847b704e867
Babu <babumaa07@gmail.com>
2023-08-07 05:58:21 +0800

    onoog oorchilj toglogchiin eeljiig solih

--- app.js
+++ app.js
@@ -18,10 +18,47 @@ document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 var diceDom = document.querySelector('.dice')
 diceDom.style.display='none';
+//Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener('click',function (){
+//1-6 доторх санамсаргүй нэг тоог гаргаж авна
+
     var diceNumber =Math.floor(Math.random() *6)+1;
+//Шооны зургийг веб дээр гаргаж ирнэ.
 
     diceDom.style.display='block';
+ //Буусан санамсаргүй тоонд харгалзах шооны зургийг вэб дээр гаргаж ирнэ
     diceDom.src = "dice-" + diceNumber + '.png';
     
+//Буусан тоо нь нэгээс ялгаатай бол идэвхтэй Тоглогчийн ээлжийн оноог нэмэгдүүлнэ
+    if(diceNumber !== 1){
+        //1ээс ялгаатай тоо буулга
+        roundScore = roundScore + diceNumber;
+        document.getElementById('current-'+ activePlayer).textContent = roundScore;
+    }
+    else{
+        //1буусан тул тоглогчийн ээлжийг энэ хэсэгт сольж өгнө
+
+//Энэ тоглогчийн ээлжиндээ цуглуулсан оноог 0 болгоно
+        roundScore = 0;
+        document.getElementById('current-'+ activePlayer).textContent = 0;
+//Тоглогчийн ээлжийг нөгөө тоглогруу шилжүүлнэ
+
+        //Хэрэв идэвхтэй тоглогч нь 0 байвал идэвхтэй тоглогчийг 1 болго
+        //Үгүй бол идэвхтэй тоглогчийг 0 болго
+
+        activePlayer === 0? activePlayer = 1 : activePlayer = 0;
+
+//улаан цэг шилшүүлэх 
+document.querySelector(".player-0-panel").classList.toggle("active");
+document.querySelector(".player-1-panel").classList.toggle("active");
+      
+//шоог түр алга болгоно
+diceDom.style.display='none';
+
+        // if(activePlayer === 0){
+        //     activePlayer = 1;
+        // }else{
+        //     activePlayer = 0;
+        // }
+    }
 });
